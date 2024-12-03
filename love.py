e2168deec2cb1f50cbe52be8828764a1a0e01843
msdanatr <111322571+msdanatr@users.noreply.github.com>
2024-12-02 21:09:29 -0800

    Delete love.py

--- love.py
+++
@@ -1,6 +0,0 @@
-#love themes words
-
-nouns = ["heart", "flower", "hum", "touch"]
-noun2 = ["soul", "spirit", "mind"]
-verbs = ["hugs", "exites", "embraces", "kisses", "consumes", "cherishes", "accepts"]
-adjectives = ["calm", "soft", "warm", "new", "loving", "tranquil", "refreshing"]
