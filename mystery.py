f2b6078dac0c10fc1ed1db9b1ed19ee6af7bfc9b
msdanatr <111322571+msdanatr@users.noreply.github.com>
2024-12-02 21:09:34 -0800

    Delete mystery.py

--- mystery.py
+++
@@ -1,6 +0,0 @@
-#mystery themes words
-
-nouns = ["branch", "eyes", "ghost", "tree", "tomb", "tower", "window", "shadow"]
-noun2 = ["path", "gate", "ground"]
-verbs = ["sways", "stands", "lurks", "rustles", "flies", "horrifies"]
-adjectives = ["sly", "dark", "spooky", "mighty", "unknown", "sneaky"]
